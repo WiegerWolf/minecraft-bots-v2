ee1239aad871e99d1986e97a9c21bca9902c5511
Your Name <you@example.com>
2026-01-30 23:21:47 +0100

    feat(viewer): add three additional bot instances for multi-bot testing
    Increase the number of FollowBot instances from two to five to better test the multi-bot visualization capabilities in the viewer.

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,6 @@ import BotWithViewer from '@/role/viewer'
 
 BotWithViewer.create(new FollowBot('nuxdie', 1))
 BotWithViewer.create(new FollowBot('nuxdie', 1))
+BotWithViewer.create(new FollowBot('nuxdie', 1))
+BotWithViewer.create(new FollowBot('nuxdie', 1))
+BotWithViewer.create(new FollowBot('nuxdie', 1))
