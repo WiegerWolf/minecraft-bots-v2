4d6a3cbb34a619174e5ed114f26eae86bf6cabfe
Your Name <you@example.com>
2026-02-02 20:38:07 +0100

    chore(bot): remove pathfinder debug bot instances

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,5 @@
 import BotWithViewer from '@/role/viewer'
-import PathfinderDebugBot from '@/role/pathfinderDebug'
 import FollowBot from './role/follow'
-import { Vec3 } from 'vec3'
-
-BotWithViewer.create(new PathfinderDebugBot(
-    new Vec3(22.5, 63, 2.5),
-    new Vec3(25.5, 69, -4.5)
-))
-BotWithViewer.create(new PathfinderDebugBot(
-    new Vec3(6.5, 69, 4.5),
-    new Vec3(6.5, 70, -9.5)
-))
-
-
 
 BotWithViewer.create(new FollowBot(
     'nuxdie'
